163ae0b0b9afa71e0058c47047abc1deaf8227cd
midjoule <47613675+midjoule@users.noreply.github.com>
2019-05-21 21:16:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,5 +49,34 @@ $(document).ready(function(){
 });
 
 
+var scrollTimeOut = true,
+    lastYPos = 0,
+    yPos = 0,
+    yPosDelta = 5,
+    nav = $('nav.navbar'),
+    navHeight = nav.outerHeight(),
+    setNavClass = function() {
+        scrollTimeOut = false;
+        yPos = $(window).scrollTop();
 
+        if(Math.abs(lastYPos - yPos) >= yPosDelta) {
+            if (yPos > lastYPos && yPos > navHeight){
+                nav.addClass('hide-nav');
+            } else {
+                nav.removeClass('hide-nav');
+            }
+            lastYPos = yPos;
+        }
+    };
+
+$(window).scroll(function(e){
+    scrollTimeOut = true;
+});
+
+setInterval(function() {
+    if (scrollTimeOut) {
+        setNavClass();
+    }
+
+}, 250);
 
